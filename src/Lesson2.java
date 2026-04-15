0fc4e15255c655543d959b7b06434b025954d210
SairaSaz <saira.sazonova87@gmail.com>
2026-04-16 02:41:25 +0400

    DZ 2.1

---
+++ src/Lesson2.java
@@ -0,0 +1,11 @@
+public class Lesson2 {
+    static void main(String [] args) {
+       printThreeWorld();
+    }
+
+    static void printThreeWorld() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
