2c58ef89a21815d4ec1ec55cc7d36f6c1c1fbe6f
SairaSaz <saira.sazonova87@gmail.com>
2026-04-16 15:59:45 +0400

    DZ 2.6.

---
+++ src/Task2_6.java
@@ -0,0 +1,13 @@
+public class Task2_6 {
+    public static void main(String[] args) {
+        printPositiveOrNegative();
+    }
+    public static void printPositiveOrNegative() {
+        int a = 5;
+        if (a >=0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
+        }
+    }
+}
