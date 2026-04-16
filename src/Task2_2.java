e7191c3471211a6c5f5177d27681bcec1d5a2cfa
SairaSaz <saira.sazonova87@gmail.com>
2026-04-17 00:14:43 +0400

    DZ 2.2

--- src/Task2_2.java
+++ src/Task2_2.java
@@ -1,14 +1,18 @@
-public class Task2_2 {public static void main(String[] args) {
-    checkSumSing();
+public class Task2_2 {
+    public static void main(String [] args) {
+    printColor();
 }
-    public static void checkSumSing() {
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+    static void printColor() {
+        int value = 5;
+        if (value < 0) {
+            System.out.println("Красный");
         }
+        else if (value > 0 && value < 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+
     }
 }
