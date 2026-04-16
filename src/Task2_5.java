27940caaee40e3b8b885ab9e9b99f96e0d329a0b
SairaSaz <saira.sazonova87@gmail.com>
2026-04-17 00:23:16 +0400

    DZ 2.5

---
+++ src/Task2_5.java
@@ -0,0 +1,19 @@
+public class Task2_5 {
+    public static void main(String[] args) {
+        isSumInRange10To20();
+    }
+    public static void isSumInRange10To20() {
+        int a = 12;
+        int b = 2;
+        int sum = a + b;
+        if (sum >10 && sum < 20) {
+            System.out.println("true");
+
+
+        }
+        else {
+            System.out.println("false");
+        }
+
+    }
+}
