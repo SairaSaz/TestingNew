4be417ddbaf35a5c67a8ff09cc6e313ae125ad39
SairaSaz <saira.sazonova87@gmail.com>
2026-04-17 00:01:46 +0400

    DZ 2.2

---
+++ src/Task2_2.java
@@ -0,0 +1,14 @@
+public class Task2_2 {public static void main(String[] args) {
+    checkSumSing();
+}
+    public static void checkSumSing() {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
