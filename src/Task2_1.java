a883978c69af472f1bfa8ab1196fb07c396aeb61
SairaSaz <saira.sazonova87@gmail.com>
2026-04-16 23:58:36 +0400

    DZ 2.1

---
+++ src/Task2_1.java
@@ -0,0 +1,10 @@
+public class Task2_1 {
+    public static void main(String [] args) {
+    printThreeWorld();
+}
+    static void printThreeWorld() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
