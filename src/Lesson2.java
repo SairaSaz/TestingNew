2571bdc19c217a605a781dcb3007c953b5849365
SairaSaz <saira.sazonova87@gmail.com>
2026-04-16 14:00:49 +0400

    DZ 2.5.

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,15 +1,17 @@
 public class Lesson2 {
     public static void main(String[] args) {
-        checkSumSing();
+        isSumInRange10To20();
     }
-    public static void checkSumSing() {
-        int a = 1;
+    public static void isSumInRange10To20() {
+        int a = 12;
         int b = 2;
         int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+        if (sum >10 && sum < 20) {
+            System.out.println("true");
         }
+        else {
+            System.out.println("false");
+        }
+
     }
 }
