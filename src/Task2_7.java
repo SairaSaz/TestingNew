267d7d134e75e88ccf1ad3435ce5ccc577d0d66c
SairaSaz <saira.sazonova87@gmail.com>
2026-04-17 16:13:35 +0400

    DZ 2.7

---
+++ src/Task2_7.java
@@ -0,0 +1,9 @@
+public class Task2_7 {
+    public static void main(String[] args) {
+        System.out.println(isNegative(-3));
+        System.out.println(isNegative(5));
+    }
+    public static boolean isNegative(int number) {
+        return number  < 0;
+    }
+    }
