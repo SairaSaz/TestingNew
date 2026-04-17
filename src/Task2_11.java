70d9c4a41e18b3fa7e250bfe5ce2115c3d4f7b31
SairaSaz <saira.sazonova87@gmail.com>
2026-04-18 02:00:23 +0400

    DZ 2.11

---
+++ src/Task2_11.java
@@ -0,0 +1,10 @@
+public class Task2_11 {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+/*Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0; */
