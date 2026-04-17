be128557d62edfa7a63630875b4c4db533089b45
SairaSaz <saira.sazonova87@gmail.com>
2026-04-17 20:39:36 +0400

    DZ 2.8

---
+++ src/Task2_8.java
@@ -0,0 +1,10 @@
+public class Task2_8 {
+    public static void main(String[] args) {
+        printWordSpecifiendNumberOfTames("I'm shocked", 5);
+    }
+    public static void printWordSpecifiendNumberOfTames (String word, int num ) {
+            for ( int i= 0; i< num; i++) {
+                System.out.println(word);
+            }
+    }
+}
