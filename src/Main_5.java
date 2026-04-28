ef0943daec1b5312c9bd68acdbc1b6654af1d55a
SairaSaz <saira.sazonova87@gmail.com>
2026-04-28 17:18:18 +0400

    DZ 5.1

--- src/Main_5.java
+++ src/Main_5.java
@@ -3,10 +3,17 @@ public class Main_5 {
         if (array.length != 4) {
             throw new MyArraySizeException("Ошибка! Строк должно быть 4");
         }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        "Ошибка! должно быть 4 столбца, а не "
+                                + array[i].length);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        String[][] table = new String[4][4];
+        String[][] table = new String[4][5];
         try {
             checkArraySize(table);
             System.out.println("Размер правильный");
